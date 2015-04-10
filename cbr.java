aeb23f745470402dbd4cc68db8987cb18288088b
jayanth.k <jayanthk19@gmail.com>
2015-04-10 23:54:17 +0530

    cbr.java

--- cbr.java
+++ cbr.java
@@ -8,70 +8,104 @@ import org.json.*;
 
 public class cbr {
 	
-	public static void main(String[] args) throws FileNotFoundException, JSONException {
+	public static void main(String[] args) throws FileNotFoundException, JSONException ,MalformedURLException , IOException {
 		
     	System.out.println("Enter your token: ");
     	Scanner scanner = new Scanner(System.in);
     	String token = scanner.nextLine();
-    	token = "CAACEdEose0cBADSZCVjhAZBZAZABkYogWugEswfygZAF3nRS1hJ2mLrLkt45WzDpsG3cZAEPb04gQa0VMYANNRg7Yfe1ZBijiRNv1o6kBsZCcO63T2iB6vaJINECBlu3bpH3Q57Ea0zFkNvEgKLEAmQM63JAG2SmpssFRPRNOIoDPk4zZAUJhZBgdN6QgahuY8HRc1yNZCZCZAvsquZBsnMltdiCwqOOuhZARTQbFcZD";
+    	token = "CAACEdEose0cBADoUZCVGhB55NZCqcnvJn7Vc3umBJVpwHe5SeusobXKBftBglwEbYmlZBTfZBHXmTvlihQTodyj0PoUZBnoQNQ0d6KuHZCStnmViy7wADeJxETOJ4JN0wyr2tiZC1i2OFZABRWIwk2pjsXPBX4vXxmWdfiKcUCscrsMIFDXMXbtH6UvLbLlqyD2N340LkDZAA2ZALX83ZCvtBBoRIZBNJnMkatcZD";
     	String url1 = "https://graph.facebook.com/me/posts?limit=1000&fields=story%2Cmessage%2Cstatus_type&access_token="+token+"&__mref=message_bubble";
     	
     	DataOutputStream out1 = new DataOutputStream(new FileOutputStream("output.txt"));
     	String json = null;
-		try {
-			
-			URL url = new URL(url1);
-		    BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
-		    String line;
-		    while ((line = in.readLine()) != null) {
-		    	//System.out.println(line);
-		    	out1.writeBytes(line);
-		    	json=line;
-		    }
-		    in.close();
-		    
-		}
-		catch (MalformedURLException e) {
-			System.out.println("Malformed URL: " + e.getMessage());
-		}
-		catch (IOException e) {
-			System.out.println("I/O Error: " + e.getMessage());
-		}
 		
-		System.out.println(json);
+		
+		URL url = new URL(url1);
+	    BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+	    String line;
+	    while ((line = in.readLine()) != null) {
+	    	//System.out.println(line);
+	    	out1.writeBytes(line);
+	    	json=line;
+	    }
+	    in.close();
+	    
+	
+		//System.out.println(json);
+	    //putting json into arrylist
 		JSONObject myjson = new JSONObject(json);
 		JSONArray the_json_array = myjson.getJSONArray("data");
-		
-		
-		
 		int size = the_json_array.length();
 		ArrayList<JSONObject> arrays = new ArrayList<JSONObject>();
 		for (int i = 0; i < size; i++) {
 	        JSONObject another_json_object = the_json_array.getJSONObject(i);
 	            arrays.add(another_json_object);
 	    }
+
+		String nexturl=null;
+		if(myjson.has("paging"))
+		{
+			JSONObject pgson = (JSONObject) myjson.get("paging");
+			nexturl= (String) pgson.get("next");
+			System.out.println(nexturl);
+		}
+		
+		get_all_page_posts(nexturl,json,arrays);
 		
 		
-		System.out.println(myjson.get("paging"));
-		JSONObject pgson = (JSONObject) myjson.get("paging");
-		System.out.println(pgson.get("next"));
 		
-		/*//System.out.println(arrays.get(0));
+		
+		
+		
+		System.out.println(arrays.get(0));
 		int arraylistsize = arrays.size();
 		int i;
 		
 		for(i=0;i<arraylistsize;i++)
 		{
-			//if(arrays.get(i).has("story"))
-			//System.out.println(arrays.get(i).get("story"));
+			if(arrays.get(i).has("story"))
+			System.out.println(arrays.get(i).get("story"));
 			
-			//if(arrays.get(i).has("message"))
-			//System.out.println(arrays.get(i).get("message")  );
-		}*/
+			if(arrays.get(i).has("message"))
+			System.out.println(arrays.get(i).get("message")  );
+		}
 		
-		
-		
-		
+	
 	}
-
+	
+	
+	public static void get_all_page_posts(String nexturl,String json,ArrayList<JSONObject> arrays) throws IOException, JSONException
+	{
+		int checkpoint=1;
+		while(checkpoint==1)
+		{
+			URL url = new URL(nexturl);
+		    BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+		    String line;
+		    line = in.readLine();
+		    in.close();
+			//System.out.println("yeah we are here");
+			//System.out.println(line);
+			
+			JSONObject obj = new JSONObject(line);
+			JSONArray obj_array = obj.getJSONArray("data");
+			int objsize=obj_array.length();
+			for (int i = 0; i < objsize; i++) 
+			{
+		        JSONObject another_json_object = obj_array.getJSONObject(i);
+		            arrays.add(another_json_object);
+		    }
+			
+			if(obj.has("paging"))
+			{
+				JSONObject pgson = (JSONObject) obj.get("paging");
+				nexturl= (String) pgson.get("next");
+				System.out.println(nexturl);
+			}
+			else
+			{
+				checkpoint= 2;
+			}
+		}	
+	}
 }
